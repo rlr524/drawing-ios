8c57287b6e7648e2a85e3580fa3d1cdac807a389
Rob Ranf <rob@emiyaconsulting.com>
2023-06-13 16:14:05 -0700

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Rob Ranf on 6/13/23.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
